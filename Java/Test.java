d8911a1c15c8c218e4f88119d1cd521c170122fa
Zheng Yune <yunethecoder@gmail.com>
2022-12-22 19:51:01 -0500

    create a java project

---
+++ Java/Test.java
@@ -0,0 +1,11 @@
+package Java;
+
+// Run a class file in java
+
+class Test {
+    public static void main(String[] args) {
+
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate"));
+    }
+}
---
+++ createJavaProject/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String firstName = "John";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+    }
+}
